281af36b336214561b7ce19d7d982c3c827ea162
Henrique Vitoi <44772383+hvitoi@users.noreply.github.com>
2022-05-24 11:23:19 -0300

    updates

--- language/java/jdk/java/util/UUID.java
+++ language/java/jdk/java/util/UUID.java
@@ -5,11 +5,18 @@ class Main {
 
     // Static methods
     UUIDRandomUUID.run();
+    UUIDFromString.fromString();
   }
 }
 
 class UUIDRandomUUID {
   static void run() {
     UUID uuid = UUID.randomUUID();
+  }
+}
+
+class UUIDFromString {
+  static void run() {
+    UUID uuid = UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
   }
 }
