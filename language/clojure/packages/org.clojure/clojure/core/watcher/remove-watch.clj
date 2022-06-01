c9ac1761fd84d81f2342a67894f2664178622c80
Henrique Vitoi <44772383+hvitoi@users.noreply.github.com>
2022-05-31 21:10:28 -0300

    updates

--- language/clojure/packages/org.clojure/clojure/core/watcher/remove-watch.clj
+++ language/clojure/packages/org.clojure/clojure/core/watcher/remove-watch.clj
@@ -1,10 +1,10 @@
 ;; react to events
 ;; e.g., react to atom values being changed
 
-(def a (atom 5))
-(add-watch a
-           :xWatcher (fn [key atom old-state new-state]
-                       (println key atom old-state new-state)))
+(def a (atom nil))
+(add-watch a :xWatcher (fn
+                         [key atom old-state new-state]
+                         (println key atom old-state new-state)))
 
 ;; change the atom state (the watcher will react)
 (reset! a 10)
