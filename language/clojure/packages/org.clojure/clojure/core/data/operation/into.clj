4a3295901c34cad22dc1de849d0e77248a7ec2aa
Henrique Vitoi <44772383+hvitoi@users.noreply.github.com>
2022-08-20 21:36:37 -0300

    updates

--- language/clojure/packages/org.clojure/clojure/core/data/operation/into.clj
+++ language/clojure/packages/org.clojure/clojure/core/data/operation/into.clj
@@ -1,4 +1,4 @@
-;; take all nasted values from the second form and insert into the map on the first form
+;; take all nested values from the second form and insert into the map on the first form
 
 (into (sorted-map) [[:a 1] [:c 3] [:b 2]])
 (into (sorted-map) [{:a 1} {:c 3} {:b 2}])
